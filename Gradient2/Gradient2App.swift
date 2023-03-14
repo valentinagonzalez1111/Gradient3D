1c5a3bc78ec1fb4f0a3035b3ea424cd5e14c6ea9
Valentina <balendi.art@gmail.com>
2023-03-14 15:23:06 +0100

    Initial Commit

---
+++ Gradient2/Gradient2App.swift
@@ -0,0 +1,17 @@
+//
+//  Gradient2App.swift
+//  Gradient2
+//
+//  Created by Apprenant 18 on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Gradient2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
