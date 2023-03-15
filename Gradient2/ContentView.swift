a60dd976ec6885c0b5233cbf35d7942e7265602c
Valentina <balendi.art@gmail.com>
2023-03-15 09:30:15 +0100

    Gradient3D

--- Gradient2/ContentView.swift
+++ Gradient2/ContentView.swift
@@ -9,18 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{ LinearGradient(colors:[.orange, .pink, .purple, .indigo], startPoint: .top, endPoint: .bottom)
+            VStack{
+                Circle()
+                    .fill(LinearGradient(colors: [.yellow, .pink], startPoint: .top, endPoint: .bottom))
+                    .frame(width: 100.0, height: 300.0)
+                    .foregroundColor(.yellow)
+                    .offset(x: 0, y: 300)
+                Circle()
+                    .fill(LinearGradient(colors: [.mint, .indigo], startPoint: .top, endPoint: .bottom))
+                    .frame(width: 150.0, height: 300.0)
+                    .foregroundColor(.mint)
+                    .offset(x: 0, y: 70)
+                Circle()
+                    .fill(LinearGradient(colors: [.pink, .purple], startPoint: .top, endPoint: .bottom))
+                    .frame(width: 200.0, height: 400.0)
+                    .foregroundColor(.pink)
+                    .offset(x: 0, y: -200)
+            }
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
